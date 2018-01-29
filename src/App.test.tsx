9640ae0ef88a032451218bff14145034e5927f0f
Kohei Hasegawa <ameutau@gmail.com>
2018-01-29 19:19:14 +0900

    Add redux-mock-store and redux-thunk to raise an error

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,12 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+const middlewares = [thunk] // add your middlewares like `redux-thunk`
+const mockStore = configureStore(middlewares)
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
